91c6521dfde736a7bdd52406b130785dcf1cd6d6
Daichi.Nakatsuji <nakatsuji.daichi@trans-cosmos.co.jp>
2025-06-09 15:13:39 +0900

    add MessageFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,14 +4,25 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Message; //追記する
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        $this->call([MessagesTableSeeder::class]);
+        // 追記する
+        Message::factory()->count(5)->create();
+        //$this->call([MessagesTableSeeder::class]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
